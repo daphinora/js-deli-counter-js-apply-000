30e7b910bf37ef554664588f508374e7ecc4ea3d
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-01-30 18:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ function nowServing(katzDeliLine) {
 
 
 function currentLine(katzDeliLine) {
-var queue = `The line is currently: `   
+var queue = `The line is currently: `
+
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.'
   }
