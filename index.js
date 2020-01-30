4f82b5a46a8adf152bf68d6e13a75061bfacb846
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-01-30 18:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ function takeANumber(deli, name) {
 
 // BACK TICKS YOU SILLY GOOSE
 
-// silly goose, you forgot the .length AGAIN. running ${deli} instead of ${deli.length} like thirty times with minor changes to the code around the actual problem is infuriarating, but what a relief to finally find the mistake (an hour later)
-
 function nowServing(katzDeliLine) {
   var i=0;
   while(i < deli.length) {
@@ -23,15 +21,8 @@ function nowServing(katzDeliLine) {
   } else 
   return(`Currently serving ${katzDeliLine.shift()}.`);
 }
-// YAY YOU REMEMBERED THE .LENGTH THIS TIME WOOT WOOT
 
-
-// post-working note: you struggled the most with currentLine. the others made sense--what problems did you have in the currentLine function? variables were difficult to understand. you weren't sure whether to treat this function with a for loop or a while loop, but you decided to choose a for loop for consistency. The base 'if' statement was easy--you'd done that a million times already, so noting those lines (28-30) went quickly. got that out of the way (despite initially forgetting to put the .length again, silly goose--didn't we just fix this problem??)
-
-// the queue.slice(0, -2) was baffling. searched through array methods for like twenty minutes (learned about different methods, though, which was cool) before realizing slice was what I needed. sigh.
-
-// **questions for this section: is there an easier (simpler, less roundabout) way to write this function? I care more about making my code work than streamlining it, but am I forming any bad habits yet?**
-
+// **questions for this next funciton: is there an easier (simpler, less roundabout) way to write this function? I care more about making my code work than streamlining it, but am I forming any bad habits yet?**
 
 function currentLine(katzDeliLine) {
 var queue = `The line is currently: `;
@@ -48,14 +39,14 @@ var queue = `The line is currently: `;
   //  var spot = i + 1
   //  var person = katzDeliLine[i]
   //  var customer = `${spot}. ${person}, `
-// yep, these work as well for simplifying purposes (checked) but why bother adding so many variables when i'm only using this code and these variables once? **which way is better practice?** probably depends on what i'm coding, eh?
+// yep, 39-41 work as well for simplifying purposes (checked) but why bother adding so many variables when i'm only using this code and these variables once? **which way is better practice?** probably depends on what i'm coding, eh?
 
 queue = queue + customer;
-// 53 stumped me. how do i run the entire array?? like this, apparently.
+// 44 stumped me. what's the difference between 53 and "queue++"
   }
 return queue.slice(0, -2);
  }
  
-// ngl i didn't know which numbers to slice in 56 so i just picked random numbers until the comma after Ann (-2) was removed and the space before 1 (0) was added. I have no idea how i did this. i think my fingers know something my brain hasn't caught up to yet. catch up, brain. (please)
+// ngl i didn't know which numbers to slice in 47 so i just picked random numbers until the comma after Ann (-2) was removed and the space before 1 (0) was added. I think it's to do with where those mistakes are at in the queue but i'm not 100% sure
 
-//it works!!!!!!!! woot woot mcswooty toot toot woot
+//   it works!!!!!!!! woot woot mcswooty toot toot woot
