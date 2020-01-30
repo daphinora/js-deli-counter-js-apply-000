0ef624a1e2046593a2c4d2676d206e286cc435d2
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-01-30 20:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  
 //deli = katzDeli from the index-test.js
 var deli = [];
-function takeANumber(deli, name) {
-  var i = 0;
+  var i = 0;function takeANumber(deli, name) {
+
   deli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${++i}.`);
 }
